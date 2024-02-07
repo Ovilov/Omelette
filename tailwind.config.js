cc3341cbde24876dadb9148179ed2047c466aed6
Ovilov <ovilovsaidakbar@gmail.com>
2024-02-08 04:23:38 +0500

    omelette add

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans"],
+      serif: ["Young Serif", "serif"],
+    },
+    extend: {
+      backgroundColor: {
+        main__pink: ["#F3E5D7"],
+        cnow: ["#FFF7FB"],
+      },
+      colors: {
+        "wenge-brown": "#5F564D",
+        raspberry: ["#7A284E"],
+        brandy: ["#854632"],
+      },
+    },
+  },
+  plugins: [],
+};
